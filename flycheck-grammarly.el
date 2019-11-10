baa73a3d5bcecbcdf4406d020b84b8fef84c0170
jenchieh <jcs090218@gmail.com>
2019-11-11 02:14:01 +0800

    Update core.

--- flycheck-grammarly.el
+++ flycheck-grammarly.el
@@ -7,7 +7,7 @@
 ;; Description: Grammarly support for Flycheck.
 ;; Keyword: grammar check
 ;; Version: 0.0.1
-;; Package-Requires: ((emacs "24.3") (flycheck "0.14"))
+;; Package-Requires: ((emacs "24.3") (flycheck "0.14") (grammarly "0.0.1"))
 ;; URL: https://github.com/jcs090218/flycheck-grammarly
 
 ;; This file is NOT part of GNU Emacs.
@@ -42,11 +42,55 @@
   :group 'flycheck
   :link '(url-link :tag "Github" "https://github.com/jcs090218/flycheck-grammarly"))
 
+(defvar-local flycheck-grammarly--done-checking nil
+  "Check if Grammarly API done checking.")
 
-;;;###autoload
-(defun flycheck-grammarly-setup ()
-  "Setup Grammarly integration for Flycheck."
-  )
+
+(defun flycheck-grammarly--on-message (data)
+  "Received DATA from Grammarly API."
+  (message "[DATA] %s" data))
+
+(defun flycheck-grammarly--on-close ()
+  "On closse Grammarly API."
+  (setq flycheck-grammarly--done-checking t)
+  (flycheck-mode 1))
+
+(defun flycheck-grammarly--after-change-functions (&rest _)
+  "After change function to check if content changes."
+  (setq flycheck-grammarly--done-checking nil))
+
+(defun flycheck-grammarly--check-all ()
+  "Check grammar for BUF document."
+  (list (list 1 2 'error "Something went wrong here.")))
+
+(defun flycheck-grammarly--start (checker callback)
+  "Flycheck start function for CHECKER, invoking CALLBACK."
+  (add-hook 'after-change-functions 'flycheck-grammarly--after-change-functions nil t)
+  (unless flycheck-grammarly--done-checking
+    (grammarly-check-text (buffer-string)))
+  (funcall
+   callback
+   'finished
+   (flycheck-increment-error-columns
+    (mapcar
+     (lambda (x)
+       (apply #'flycheck-error-new-at `(,@x :checker ,checker)))
+     (condition-case err
+         (if flycheck-grammarly--done-checking
+             (flycheck-grammarly--check-all)
+           (flycheck-mode -1))
+       (error
+        (funcall callback 'errored (error-message-string err))
+        (signal (car err) (cdr err))))))))
+
+(flycheck-define-generic-checker 'grammarly-checker
+  "Grammarly flycheck definition."
+  :start #'flycheck-grammarly--start
+  :modes '(text-mode latex-mode org-mode markdown-mode))
+
+(add-to-list 'flycheck-checkers 'grammarly-checker)
+(add-to-list 'grammarly-on-message-function-list 'flycheck-grammarly--on-message)
+(add-to-list 'grammarly-on-close-function-list 'flycheck-grammarly--on-close)
 
 
 (provide 'flycheck-grammarly)
