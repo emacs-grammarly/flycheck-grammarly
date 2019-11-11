bff2d9f50f0c7829634e904889faa31700167706
Jen-Chieh <jcs090218@gmail.com>
2019-11-11 16:59:49 +0800

    Add open callback.

--- flycheck-grammarly.el
+++ flycheck-grammarly.el
@@ -7,7 +7,7 @@
 ;; Description: Grammarly support for Flycheck.
 ;; Keyword: grammar check
 ;; Version: 0.0.1
-;; Package-Requires: ((emacs "24.4") (flycheck "0.14") (grammarly "0.0.1") (cl-lib "0.6") (s "1.12.0"))
+;; Package-Requires: ((emacs "25.1") (flycheck "0.14") (grammarly "0.0.1") (cl-lib "0.6") (s "1.12.0"))
 ;; URL: https://github.com/jcs090218/flycheck-grammarly
 
 ;; This file is NOT part of GNU Emacs.
@@ -68,7 +68,7 @@
 
 (defun flycheck-grammarly--on-open ()
   "On open Grammarly API."
-  )
+  (message "[INFO] Start connecting to Grammarly API..."))
 
 (defun flycheck-grammarly--on-message (data)
   "Received DATA from Grammarly API."
