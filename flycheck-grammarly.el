7816b84b7e52973a129053d8d302f5b529b2e314
Jen-Chieh <jcs090218@gmail.com>
2020-10-28 14:45:47 +0800

    Update dep.

--- flycheck-grammarly.el
+++ flycheck-grammarly.el
@@ -7,7 +7,7 @@
 ;; Description: Grammarly support for Flycheck.
 ;; Keyword: grammar check
 ;; Version: 0.2.2
-;; Package-Requires: ((emacs "25.1") (flycheck "0.14") (grammarly "0.1.3"))
+;; Package-Requires: ((emacs "25.1") (flycheck "0.14") (grammarly "0.3.0"))
 ;; URL: https://github.com/jcs-elpa/flycheck-grammarly
 
 ;; This file is NOT part of GNU Emacs.
