974f1c1d2a5631425c1b341a3f9f4a6de69782fc
Jen-Chieh Shen <jcs090218@gmail.com>
2022-06-17 01:42:29 +0800

    chore(pkg): info

--- flycheck-grammarly.el
+++ flycheck-grammarly.el
@@ -4,11 +4,10 @@
 ;; Created date 2019-11-06 18:08:01
 
 ;; Author: Shen, Jen-Chieh <jcs090218@gmail.com>
-;; Description: Grammarly support for Flycheck.
-;; Keyword: grammar check
+;; URL: https://github.com/emacs-grammarly/flycheck-grammarly
 ;; Version: 0.2.3
 ;; Package-Requires: ((emacs "25.1") (flycheck "0.14") (grammarly "0.3.0") (s "1.12.0"))
-;; URL: https://github.com/emacs-grammarly/flycheck-grammarly
+;; Keywords: grammar check
 
 ;; This file is NOT part of GNU Emacs.
 
